37913e17d4aadcc395e29a3bbdfa7b29ba9fd4f5
MrDuDe98 <m.a.gomes230@gmail.com>
2018-03-07 21:51:40 +0000

    ficha2

--- aula2/ficha2.js
+++ aula2/ficha2.js
@@ -2,24 +2,38 @@ function main(){
 
     //linha 1
 
-    imc(80,1.84);
+    imc(80,1.40);
 
+    //linha 2
+    inverso("ola mundo");
 }
 
 //Linha1
 function imc(peso, altura){
     var imc = 0;
     imc = peso / Math.pow(altura, 2 );
-    return(imc);
+    console.log("O seu IMC é: ",imc)
     if(imc <= 18.5){
         console.log("abaixo de peso")
-    }if(18.5 <= imc <=25 ){
+    }if(imc > 18.5 && imc <= 25 ){
         console.log("peso normal")
-    }if(25 <= imc <=30){
+    }if(imc > 25 && imc <= 30){
         console.log("acima do peso")
     }if(imc > 30){
         console.log("obeso")
     }
+}
+
+//linha2
+function inverso(frase){
+    var fraseInv = "";
+    var l = frase.length - 1;
+    for(i = l; i >= 0; i--){
+        fraseInv += frase.charAt(i);
+    }
+    
+
+console.log(fraseInv);
 }
 
 //INLINE
