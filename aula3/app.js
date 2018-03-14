96bef708c8e9475890fd0c9d87c8b56c5548c37c
MrDuDe98 <m.a.gomes230@gmail.com>
2018-03-14 18:08:31 +0000

    aula3

--- aula3/app.js
+++ aula3/app.js
@@ -1,1 +1,15 @@
-{\rtf1}
+var greetMe = function() {
+    console.log("Hello");
+}
+var greetWorld = function() {
+    console.log("World!")
+}
+
+function log(fn){
+    return greetMe;
+}
+
+
+greetMe();
+greetWorld();
+log(greetMe);
