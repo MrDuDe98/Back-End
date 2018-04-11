512a2ef8b7743e321dda7913a225ee9a88f64f9b
MrDuDe98 <m.a.gomes230@gmail.com>
2018-04-11 18:49:10 +0100

    aula 5

--- aula4/app.js
+++ aula4/app.js
@@ -18,5 +18,20 @@ console.log(json);
 var text = '{ "name":"Marco", "age":"19", "gender":"Masculino" }';
 var str = JSON.parse(text);
 console.log(str.name + ", " + str.age + ", " + str.gender);
+console.log(" ");
+console.log("EXERCICIO 5");
+console.log(" ");
+var Emitter = require("./emitter");
+var eventConstants = require ("./constants");
+var emtr = new Emitter();
 
-        //exercicio 3.
+emtr.on(eventConstants.events.FILESAVED, function(){
+    console.log("Somewhere, someone said hello?");
+});
+
+emtr.on(eventConstants.events.GREET, function(){
+    console.log("A greeting ocured");
+});
+
+emtr.emit(eventConstants.events.FILESAVED);
+emtr.emit(eventConstants.events.GREET);
